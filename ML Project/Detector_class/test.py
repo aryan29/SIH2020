a649d1b9c88e4ef58e1c564f841df4a293d9042d
Abhilasha <abhisinha662000@gmail.com>
2020-06-05 00:27:43 +0530

    create garbage_detector.py

---
+++ ML Project/Detector_class/test.py
@@ -0,0 +1,7 @@
+import garbage_detector as gd
+
+if __name__ == "__main__":
+    obj = gd.GarbageDetector()
+    ans = obj.detect('C:\\Users\\Abhilasha\\Pictures\\test_data\\garbage\\c3d74078-6799-11e5-ba3c-40f2e96c8ad8.jpg')
+    print(ans)
+    
