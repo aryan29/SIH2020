8291b9eb26c9573f3add26d96a3e0e23758669e9
Abhilasha Sinha <43816262+Abhilasha06@users.noreply.github.com>
2020-07-27 01:49:28 +0530

    looping over points

--- Plotting_on_map/heat_map/app.js
+++ Plotting_on_map/heat_map/app.js
@@ -13,29 +13,19 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), mapOptions);*/
 
  /* Data points defined as a mixture of WeightedLocation and LatLng objects */
-var heatMapData = [
-  {location: new google.maps.LatLng(37.782, -122.447), weight: 0.5},
-  new google.maps.LatLng(37.782, -122.445),
-  {location: new google.maps.LatLng(37.782, -122.443), weight: 2},
-  {location: new google.maps.LatLng(37.782, -122.441), weight: 3},
-  {location: new google.maps.LatLng(37.782, -122.439), weight: 2},
-  new google.maps.LatLng(37.782, -122.437),
-  {location: new google.maps.LatLng(37.782, -122.435), weight: 0.5},
+ var heatMapData = [];
+ var data = [{"index": 2, "latitude": 80.0, "longitude": 50.0}, {"index": 35, "latitude": 27.0, "longitude": 90.0}, {"index": 80, "latitude": 31.14, "longitude": 75.54}, {"index": 35, "latitude": 31.0, "longitude": 75.5}, {"index": 7, "latitude": 31.14, "longitude": 72.54}, {"index": 40, "latitude": 31.0, "longitude": 70.5}];
+ for (var i = 0; i < data.length; i++) 
+ {
+   heatMapData[i] = ({location: new google.maps.LatLng(data[i].latitude, data[i].longitude), weight: data[i].index});
+   
+ }
 
-  {location: new google.maps.LatLng(37.785, -122.447), weight: 3},
-  {location: new google.maps.LatLng(37.785, -122.445), weight: 2},
-  new google.maps.LatLng(37.785, -122.443),
-  {location: new google.maps.LatLng(37.785, -122.441), weight: 0.5},
-  new google.maps.LatLng(37.785, -122.439),
-  {location: new google.maps.LatLng(37.785, -122.437), weight: 2},
-  {location: new google.maps.LatLng(37.785, -122.435), weight: 3}
-];
-
-var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
+var sanFrancisco = new google.maps.LatLng(30, 75);
 
 map = new google.maps.Map(document.getElementById('map'), {
   center: sanFrancisco,
-  zoom: 13,
+  zoom: 5,
   //mapTypeId: 'satellite'
 });
 
